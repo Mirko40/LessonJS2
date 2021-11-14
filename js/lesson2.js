c8025efaa9596d5f90634de0aefc3c1fd4468d00
Mirko40 <mmirko@ua.fm>
2021-11-14 19:36:04 +0200

    initial

--- js/lesson2.js
+++ js/lesson2.js
@@ -16,23 +16,23 @@ function getSymbol() {
     let symbolNumber = +prompt("Enter a number between 0 and 9");
     if (symbolNumber == 0) {
         alert(`0-)`);
-    } else if (symbolNumber == 1) {
+    } else if (symbolNumber === 1) {
         alert(`1-!`);
-    } else if (symbolNumber == 2) {
+    } else if (symbolNumber === 2) {
         alert(`2-@`);
-    } else if (symbolNumber == 3) {
+    } else if (symbolNumber === 3) {
         alert(`3-#`);
-    } else if (symbolNumber == 4) {
+    } else if (symbolNumber === 4) {
         alert(`4-$`);
-    } else if (symbolNumber == 5) {
+    } else if (symbolNumber === 5) {
         alert(`5-%`);
-    } else if (symbolNumber == 6) {
+    } else if (symbolNumber === 6) {
         alert(`6-^`);
-    } else if (symbolNumber == 7) {
+    } else if (symbolNumber === 7) {
         alert(`7-&`);
-    } else if (symbolNumber == 8) {
+    } else if (symbolNumber === 8) {
         alert(`8-*`);
-    } else if (symbolNumber == 9) {
+    } else if (symbolNumber === 9) {
         alert(`9-(`);
     } else alert(`Enter a single digit`);
 };
@@ -42,7 +42,7 @@ function getIdentialNumbers() {
     let a = (Math.trunc(getNumber / 100));
     let b = (Math.trunc((getNumber - a * 100) / 10));
     let c = (getNumber - a * 100 - b * 10);
-    if (a == b || a == c || b == c) {
+    if (a === b || a === c || b === c) {
         alert(`There are the same numbers in this number`);
     } else alert(`There are no identical numbers`);
 };
@@ -53,8 +53,47 @@ function getIdentialNumbers2() {
     let a = str[0];
     let b = str[1];
     let c = str[2];
-    if (a == b || a == c || b == c) {
+    if (a === b || a === c || b === c) {
         alert(`There are the same numbers in this number`);
     } else alert(`There are no identical numbers`);
 };
 
+function getLeapYear() {
+    let getYear = +prompt("Enter the year which you want to check for a leap year");
+
+
+    if (getYear % 400 === 0 || getYear % 4 === 0 && getYear % 100 != 0) {
+        alert(`This is a leap year`);
+    } else alert(`There is a not a leap year`);
+};
+
+function getNumberCheck() {
+    let getNumber = +prompt("Enter a five-digit number to check?");
+    let str = String(getNumber);
+    let strLenght = str.length;
+    let a = str[0];
+    let b = str[1];
+    let d = str[3];
+    let e = str[4];
+    if (strLenght != 5) {
+        alert(`This is an incorrect number`);
+    } else if (a === e && b === d) {
+        alert(`This number is a palyndrome`);
+    } else alert(`This number is a not palyndrome`);
+};
+
+function exchDollar() {
+    let cursEUR = 0.86;
+    let cursUAH = 26.17;
+    let cursAZN = 1.7;
+    let howDollars = +prompt("How many dollars do you want to convert?");
+    let currencyConvert = prompt("In what currency(EUR, UAH, AZN) do you want to convert dollars?");
+    let currency = String(currencyConvert);
+    if (currency === 'EUR') {
+        alert(`You have ${cursEUR * howDollars} EUR`);
+    } else if (currency === 'UAH') {
+        alert(`You have ${cursUAH * howDollars} UAH`);
+    } else if (currency === 'AZN') {
+        alert(`You have ${cursAZN * howDollars} AZN`);
+    } else alert(`Enter the correct currency for the exchange`);
+};
